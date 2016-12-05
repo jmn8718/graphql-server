e0b005f86279349f8828d06e5c8cdbea0a1245cd
Jose Navarro <jose.navarro@famoco.com>
2016-12-05 17:50:53 +0100

    modified query to aggregate results

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,35 @@ import { Test } from '../data/connectors';
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  Test.find({}, (err, data) => {
-    console.log(err, data)
+  Test.aggregate([
+    { $group: {
+      _id: "$identifiers.build_serial",
+      dataY: {
+        $push: "$results.percentage"
+      },
+      dataX: {
+        $push: "$results.time_stamp"
+      }
+    }}
+  ], (err, results) => {
     if (err) {
       next(err)
     }
-    res.render('graph', { title: 'SERVER', data: data });
+    const series = results.map((test) => {
+      const data = []
+      for(let i = 0; i < test.dataX.length; i += 1) {
+        const x = parseInt(test.dataX[i], 10)
+        const y = test.dataY[i]
+        if (x && y && x !== null && y !== null) {
+          data.push([x, y])
+        }
+      }
+      return {
+        name: test._id,
+        data,
+      }
+    });
+    res.render('graph', { title: 'SERVER', series: series });
   })
 });
 
